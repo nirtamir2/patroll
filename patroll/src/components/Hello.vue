0b33f1b135dd58f8b7484059da1625edbd41f05a
Nir Tamir <nirtamir2@gmail.com>
2017-08-29 21:40:40 +0300

    get current location

--- patroll/src/components/Hello.vue
+++ patroll/src/components/Hello.vue
@@ -1,52 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="getCurrentLocation">get location</button>
+    <span>
+      <h1>
+        {{this.longitude}}, {{this.latitude}}
+      </h1>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
+  name: 'app',
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      longitude: "",
+      latitude: ""
+    }
+  },
+  methods: {
+    getCurrentLocation() {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          [this.latitude, this.longitude] = [position.coords.latitude, position.coords.longitude];
+          console.log(position.coords.latitude, position.coords.longitude);
+        }
+        )
+      }
+    },
+    watchCurrentLocation() {
+      if ("geolocation" in navigator) {
+        navigator.geolocation.watchPosition((position) => {
+          [this.latitude, this.longitude] = [position.coords.latitude, position.coords.longitude];
+          console.log(position.coords.latitude, position.coords.longitude);
+        })
+      }
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
-</style>
